ca8092104b7730c72acb0ebee449bfd7f7029a28
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2021-11-08 18:29:12 +0500

    Update 1.5.cpp

--- 1.5.cpp
+++ 1.5.cpp
@@ -1,19 +1,15 @@
-﻿/*
+/*
 ID: 54672943
 Вывести квадраты натуральных чисел от 1 до n, используя только O(n) операций сложения и вычитания (умножением пользоваться нельзя). n ≤ 1000.
+Ассимптотика: O(n)
+Память: T(1)
 */
 #include <iostream>
 
-void Solve()
+void Solve(int& num, int& d)
 {
-    int n, num = 0, d = 1;
-    std::cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        num += d;
-        d += 2;
-        std::cout << num << " ";
-    }
+    num += d;
+    d += 2;
 }
 
 int main()
@@ -21,11 +17,14 @@ int main()
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
-    Solve();
+    int n, num = 0, d = 1;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        Solve(num, d);
+        std::cout << num << " ";
+    }   
+    std::cout << "\n";
     return 0;
 }
-/*
-Ассимптотика: O(n)
-Память: T(1)
-*/
 
