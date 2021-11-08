5768f025ecda68f06cf6a9ee2a789a46734e5e0f
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2021-11-08 18:37:29 +0500

    Update 6_3.cpp

--- 6_3.cpp
+++ 6_3.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ID: 55369386
 На вокзале есть некоторое количество тупиков, куда прибывают электрички. 
 Этот вокзал является их конечной станцией. Дано расписание движения электричек, в котором для каждой электрички указано время ее прибытия, 
@@ -7,28 +7,23 @@ ID: 55369386
 которая прибывает в момент времени X, в этот тупик ставить нельзя, а электричку, прибывающую в момент X+1 — можно.
 В данный момент на вокзале достаточное количество тупиков для работы по расписанию. Напишите программу, которая по данному расписанию определяет,
 какое минимальное количество тупиков требуется для работы вокзала.
+Асимптотика: O(n)
+Память: O(n)
 */
 #include <iostream>
 #include <vector>
 #include <queue>
 
-int Solve()
-{
-    int n;
-    std::cin >> n;
-    int arr, dep, ans = 1;
-    std::priority_queue<int, std::vector<int>, std::greater<int>> deadEnds;
-    std::cin >> arr >> dep;
-    deadEnds.push(dep);
-    for (int i = 1; i < n; i++)
+int Solve(std::priority_queue<int, std::vector<int>, std::greater<int>> deadEnds, int ans, int arr, int dep)
+{       
+    if (deadEnds.top() < arr)
     {
-        std::cin >> arr >> dep;
-        if (deadEnds.top() < arr)
-        {
-            deadEnds.pop(); // если есть свободный тупик
-        }
-        deadEnds.push(dep);
-        ans = std::max((int)deadEnds.size(), ans);
+            deadEnds.pop();  // если есть свободный тупик
+    }
+    deadEnds.push(dep);
+    if (deadEnds.size() > ans)
+    {
+        ans = deadEnds.size();
     }
     return ans;
 }
@@ -37,10 +32,17 @@ int main() {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
-    std::cout << Solve();
+    int n, arr, dep, ans = 1;;
+    std::cin >> n;
+
+    std::priority_queue<int, std::vector<int>, std::greater<int>> deadEnds;
+    std::cin >> arr >> dep;
+    deadEnds.push(dep);
+    for (int i = 1; i < n; i++)
+    {
+        std::cin >> arr >> dep;
+        ans = Solve(deadEnds, ans, arr, dep);
+    }
+    std::cout << ans << '\n';
     return 0;
 }
-/*
-Ассимптотика: O(n)
-Память: O(n)
-*/
