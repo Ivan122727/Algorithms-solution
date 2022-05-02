83e2cbfa1fb4ab1091f6ca7ed8584cc060eff7f8
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-05-02 16:28:44 +0500

    1.1.6

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "BigInteger.h"
+
+int main()
+{
+	BigInteger a("12133423242454252352"), b = 13;
+	std::cout << a + b << "\n";
+	std::cout << a - b << "\n";
+	std::cout << a / b << "\n";
+	std::cout << a * b << "\n";
+	if (a)
+	{
+		std::cout << "Not equal Zero";
+	}
+	return 0;
+}
