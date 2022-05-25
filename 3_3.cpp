2079e117e751cbf4d8b4d80910bcc23cbd6df3cf
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-05-25 10:23:44 +0500

    Update 3_3.cpp

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 В дощечке в один ряд вбиты гвоздики.
 Любые два гвоздика можно соединить ниточкой.
 Требуется соединить некоторые пары гвоздиков ниточками так, чтобы к каждому гвоздику была привязана хотя бы одна ниточка, а суммарная длина всех ниточек была минимальна.
@@ -10,24 +10,29 @@ ID: 65420291
 #include <iostream>
 #include <vector>
 
+int Solve(std::vector<std::pair<int, int>>& dp, std::vector<int>& pos, int n)
+{
+    std::sort(pos.begin(), pos.end());
+    dp[1].first = 1e9;
+    dp[1].second = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i].first = std::min(dp[i - 1].first, dp[i - 1].second) + (pos[i] - pos[i - 1]);
+        dp[i].second = dp[i - 1].first;
+    }
+    return dp[n].first;
+}
+
 int main()
 {
     int n;
     std::cin >> n;
-    int dp[101][2];
+    std::vector<std::pair<int, int>> dp(n + 1);
     std::vector<int> pos(n + 1);
     for (int i = 1; i <= n; i++)
     {
         std::cin >> pos[i];
     }
-    std::sort(pos.begin(), pos.end());
-    dp[1][0] = 1e9;
-    dp[1][1] = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i][0] = std::min(dp[i - 1][0], dp[i - 1][1]) + (pos[i] - pos[i - 1]);
-        dp[i][1] = dp[i - 1][0];
-    }
-    std::cout << dp[n][0];
+    std::cout << Solve(dp, pos, n) << "\n";
     return 0;
 }
