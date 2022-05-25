6bf86555ef865fc993c623910f458af2da856a71
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-05-25 10:27:19 +0500

    Update 2_2.cpp

--- 2_2.cpp
+++ 2_2.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Условия:
 В большой IT-фирме есть только одна переговорная комната.
 Желающие посовещаться заполняют заявки с желаемым временем начала и конца.
@@ -17,6 +17,21 @@ bool cmp(const std::pair<int, int>& a, const std::pair<int, int>& b)
 	return a.second < b.second;
 }
 
+int Solve(std::vector<std::pair<int, int>>& events)
+{
+	std::sort(events.begin(), events.end(), cmp);
+	int j = 0, ans = 1;
+	for (int i = 1; i < events.size(); i++)
+	{
+		if (events[i].first >= events[j].second)
+		{
+			ans++;
+			j = i;
+		}
+	}
+	return ans;
+}
+
 int main()
 {
 	std::ios::sync_with_stdio(0);
@@ -28,16 +43,6 @@ int main()
 	{
 		events.push_back({ start, finish });
 	}
-	std::sort(events.begin(), events.end(), cmp);
-	int j = 0, ans = 1;
-	for (int i = 1; i < events.size(); i++)
-	{
-		if (events[i].first >= events[j].second)
-		{
-			ans++;
-			j = i;
-		}
-	}
-	std::cout << ans << "\n";
+	std::cout << Solve(events) << "\n";
 	return 0;
 }
