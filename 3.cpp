c3a8e825295ff93a928f07f782eeafe051076606
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-06-01 13:01:21 +0500

    2.2.3

--- 3.cpp
+++ 3.cpp
@@ -1,66 +1,141 @@
 ﻿/*
-Дан невзвешенный неориентированный граф.
-Определить, является ли он двудольным. Требуемая сложность O(V+E)
-Асимптотика: T(V, E) = O(V + E)
+Дан ориентированный граф.
+Определите, какое минимальное число ребер необходимо добавить, чтобы граф стал сильносвязным.
+В графе возможны петли
+Асимптотика: T(n) = O(n)
 Память: M(n) = O(n)
-ID: 67964077
+ID: 67964254
 */
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
-void DFS(int v, int curColor, std::vector<std::vector<int>>& g, std::vector<int>& color, bool& bipartiteGraph)
+void DFS1(int v, std::vector<bool>& used, std::vector<int>& order, std::vector<std::vector<int>>& g1)
 {
-    if (!bipartiteGraph)
+    used[v] = true;
+    for (int i = 0; i < g1[v].size(); i++)
     {
-        return;
-    }
-    color[v] = curColor;
-    for (auto x : g[v])
-    {
-        if (!color[x])
+        if (!used[g1[v][i]])
         {
-            if (curColor == 1)
-            {
-                DFS(x, 2, g, color, bipartiteGraph);
-            }
-            else if (curColor == 2)
-            {
-                DFS(x, 1, g, color, bipartiteGraph);
-            }
+            DFS1(g1[v][i], used, order, g1);
         }
-        else
+    }
+    order.push_back(v);
+}
+
+void DFS2(int v, std::vector<bool>& used, std::vector<int>& component, std::vector<std::vector<int>>& g2)
+{
+    used[v] = true;
+    component.push_back(v);
+    for (int i = 0; i < g2[v].size(); i++)
+    {
+        if (!used[g2[v][i]])
         {
-            if (color[v] == color[x])
-            {
-                bipartiteGraph = false;
-                return;
-            }
+            DFS2(g2[v][i], used, component, g2);
         }
     }
 }
 
 int main()
 {
+    std::vector<int> order;
+    std::vector<int> component;
+    std::vector<std::pair<int, int>> isST;
     int n, m;
     std::cin >> n >> m;
-    std::vector<std::vector<int>> g(n);
-    std::vector<int> color(n);
-    int a, b;
+    if (n == 1)
+    {
+        std::cout << 0;
+        return 0;
+    }
+    std::vector<std::vector<int>> g1(n), g2(n);
     for (int i = 0; i < m; i++)
     {
+        int a, b;
         std::cin >> a >> b;
-        g[a].push_back(b);
-        g[b].push_back(a);
+        if (a == b)
+        {
+            continue;
+        }
+        a--;
+        b--;
+        bool isSecondRoad = false;
+        for (int j = 0; j < g1[a].size(); j++)
+        {
+            if (g1[a][j] == b)
+            {
+                isSecondRoad = true;
+                break;
+            }
+        }
+        if (!isSecondRoad)
+        {
+            g1[a].push_back(b);
+            g2[b].push_back(a);
+        }
     }
-    bool bipartiteGraph = true;
-    DFS(0, 1, g, color, bipartiteGraph);
-    if (bipartiteGraph)
+    std::vector<bool> used(n, false);
+    for (int i = 0; i < n; i++)
     {
-        std::cout << "YES";
+        if (!used[i])
+        {
+            DFS1(i, used, order, g1);
+        }
     }
-    else
+    used.assign(n, false);
+    std::vector<int> dsu(n);
+    int cursor = 0;
+    for (int i = 0; i < n; i++)
     {
-        std::cout << "NO";
+        int v = order[n - 1 - i];
+        if (!used[v])
+        {
+            DFS2(v, used, component, g2);
+            if (component.size() == n)
+            {
+                std::cout << 0;
+                return 0;
+            }
+            for (auto x : component)
+            {
+                dsu[x] = cursor;
+            }
+            component.clear();
+            cursor++;
+        }
     }
+    isST.resize(cursor, { 0, 0 });
+    used.assign(n, false);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < g1[i].size(); j++)
+        {
+            if (dsu[i] != dsu[g1[i][j]])
+            {
+                isST[dsu[i]].first++;
+                isST[dsu[g1[i][j]]].second++;
+            }
+        }
+    }
+    int s = 0, t = 0, q = 0;
+    for (int i = 0; i < cursor; i++)
+    {
+        if (!isST[i].first && !isST[i].second)
+        {
+            q++;
+        }
+        else if ((isST[i].first || isST[i].second) && isST[i].first - isST[i].second != 0)
+        {
+            if (isST[i].first)
+            {
+                s++;
+            }
+            if (isST[i].second)
+            {
+                t++;
+            }
+        }
+    }
+    std::cout << std::max(s + q, t + q) << "\n";
     return 0;
 }
