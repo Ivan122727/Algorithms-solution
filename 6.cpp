d89e8bfac335eb15b3c26c4f5868f10c58d03f5a
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-06-01 12:50:13 +0500

    2.2.6

--- 6.cpp
+++ 6.cpp
@@ -1,24 +1,53 @@
-/*
-ID: 66223102
+﻿/*
+Найти в связном графе остовное дерево минимального веса.
+Асимптотика: T(n) = O(n)
+Память: M(n) = O(n)
+ID:	68007987
 */
 #include <iostream>
-#include "BigInteger.h"
+#include <set>
+#include <vector>
 
 int main()
 {
-	BigInteger a("12133423242454252352"), b = 13;
-	std::cout << a + b << "\n";
-	std::cout << a - b << "\n";
-	std::cout << a / b << "\n";
-	std::cout << a * b << "\n";
-	if(a)
+	const int N = 100000;
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<std::pair<int, int>> g[N];
+	for (int i = 0; i < m; i++)
 	{
-		std::cout << "Not equal Zero\n";
+		int a, b, c;
+		std::cin >> a >> b >> c;
+		a--;
+		b--;
+		g[a].push_back({ b, c });
+		g[b].push_back({ a, c });
 	}
-	a = 0;
-	if (!a)
+	std::vector<int> dist(n), was(n);
+	for (int i = 0; i < n; i++)
 	{
-		std::cout << "Equal Zero\n";
+		dist[i] = 1e9;
+		was[i] = 0;
 	}
-	return 0;
+	dist[0] = 0;
+	std::set<std::pair<int, int>> s;
+	s.insert({ 0, 0 });
+	int costMst = 0;
+	while (!s.empty())
+	{
+		auto [distanse, v] = *s.begin();
+		costMst += distanse;
+		s.erase(s.begin());
+		was[v] = 1;
+		for (auto [u, cost] : g[v])
+		{
+			if (dist[u] > cost && was[u] == 0)
+			{
+				s.erase({ dist[u], u });
+				dist[u] = cost;
+				s.insert({ dist[u], u });
+			}
+		}
+	}
+	std::cout << costMst << "\n";
 }
