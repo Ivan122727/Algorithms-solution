05b5e3a7213b3294cadc7ce377e27e37ed9ff8e8
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-06-07 22:56:34 +0300

    Update 2.3.cpp

--- 2.3.cpp
+++ 2.3.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Дан невзвешенный неориентированный граф.
 Определить, является ли он двудольным. Требуемая сложность O(V+E)
 Асимптотика: T(V, E) = O(V + E)
@@ -8,14 +8,59 @@ ID: 67964077
 #include <iostream>
 #include <vector>
 
-void DFS(int v, int curColor, std::vector<std::vector<int>>& g, std::vector<int>& color, bool& bipartiteGraph)
+class ListGraph
+{
+public:
+    ListGraph(int size);
+    virtual void AddEdge(int from, int to);
+    virtual int VerticesCount() const;
+    virtual void FindAllAdjacentIn(int vertex, std::vector<int>& vertices) const;
+    virtual void FindAllAdjacentOut(int vertex, std::vector<int>& vertices) const;
+private:
+    int size;
+    std::vector<std::vector<int>> g;
+    std::vector<std::vector<int>> reversedG;
+};
+
+ListGraph::ListGraph(int size) : size(size), g(std::vector<std::vector<int>>(size, std::vector<int>())), reversedG(std::vector<std::vector<int>>(size, std::vector<int>())) {}
+
+void ListGraph::AddEdge(int from, int to)
+{
+    g[from].push_back(to);
+    reversedG[to].push_back(from);
+}
+
+int ListGraph::VerticesCount() const
+{
+    return size;
+}
+
+void ListGraph::FindAllAdjacentIn(int vertex, std::vector<int>& vertices) const
+{
+    for (int x : reversedG[vertex])
+    {
+        vertices.push_back(x);
+    }
+}
+
+void ListGraph::FindAllAdjacentOut(int vertex, std::vector<int>& vertices) const 
+{
+    for (int i : g[vertex])
+    {
+        vertices.push_back(i);
+    }
+}
+
+void DFS(int v, int curColor, ListGraph& g, std::vector<int>& color, bool& bipartiteGraph)
 {
     if (!bipartiteGraph)
     {
         return;
     }
     color[v] = curColor;
-    for (auto x : g[v])
+    std::vector<int> vertices;
+    g.FindAllAdjacentIn(v, vertices);
+    for (auto& x : vertices)
     {
         if (!color[x])
         {
@@ -43,24 +88,24 @@ int main()
 {
     int n, m;
     std::cin >> n >> m;
-    std::vector<std::vector<int>> g(n);
+    ListGraph g(n);
     std::vector<int> color(n);
     int a, b;
     for (int i = 0; i < m; i++)
     {
         std::cin >> a >> b;
-        g[a].push_back(b);
-        g[b].push_back(a);
+        g.AddEdge(a, b);
+        g.AddEdge(b, a);
     }
     bool bipartiteGraph = true;
     DFS(0, 1, g, color, bipartiteGraph);
     if (bipartiteGraph)
     {
-        std::cout << "YES";
+        std::cout << "YES\n";
     }
     else
     {
-        std::cout << "NO";
+        std::cout << "NO\n";
     }
     return 0;
 }
