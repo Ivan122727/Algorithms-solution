e193558416d9299f569b1d896ffb1887be2b2915
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-06-08 21:07:24 +0500

    2.3.1

---
+++ 2_1.cpp
@@ -0,0 +1,54 @@
+﻿/*
+Найти лексикографически-минимальную строку, построенную по префикс-функции, в алфавите a-z.
+Время: T(n) = O(n)
+Память: M(n) = O(n)
+ID: 68779417
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string BuildString(const std::vector<int>& p)
+{
+    if (p.size() == 0) 
+    {
+        return "";
+    }
+    std::string ans = "a";
+    for (int i = 1; i < p.size(); i++) 
+    {
+        if (p[i] != 0) 
+        {
+            ans += ans[p[i] - 1];
+        }
+        else 
+        {
+            std::vector<bool> alphabet(26, false);
+            int k = p[i - 1];
+            while (k > 0) 
+            {
+                alphabet[ans[k] - 'a'] = true;
+                k = p[k - 1];
+            }
+            char c = 'b';
+            while (alphabet[c - 'a']) 
+            {
+                c++;
+            }
+            ans += c;
+        }
+    }
+    return ans;
+}
+
+int main() 
+{
+    int num;
+    std::vector<int> p;
+    while(std::cin >> num) 
+    {
+        p.push_back(num);
+    }
+    std::cout << BuildString(p) << "\n";
+    return 0;
+}
