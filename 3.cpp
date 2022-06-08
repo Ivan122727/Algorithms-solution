041d634b5dd49f1eb53ddba48e64940c1d91b231
Ivan122727 <64577263+Ivan122727@users.noreply.github.com>
2022-06-08 14:19:24 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Дан ориентированный граф.
 Определите, какое минимальное число ребер необходимо добавить, чтобы граф стал сильносвязным.
 В графе возможны петли
@@ -9,71 +9,108 @@ ID: 67964254
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <stack>
 
-void DFS1(int v, std::vector<bool>& used, std::vector<int>& order, std::vector<std::vector<int>>& g1)
+class ListGraph
 {
-    used[v] = true;
-    for (int i = 0; i < g1[v].size(); i++)
-    {
-        if (!used[g1[v][i]])
-        {
-            DFS1(g1[v][i], used, order, g1);
-        }
-    }
-    order.push_back(v);
+public:
+    ListGraph(int size);
+    virtual void AddEdge(int from, int to);
+    virtual int VerticesCount() const;
+    virtual int NeighboursCount(int vertex) const;
+    virtual void FindAllAdjacentIn(int vertex, std::vector<int>& vertices) const;
+    virtual void FindAllAdjacentOut(int vertex, std::vector<int>& vertices) const;
+private:
+    int size;
+    std::vector<std::vector<int>> g;
+    std::vector<std::vector<int>> reversedG;
+};
+
+ListGraph::ListGraph(int size) : size(size), g(std::vector<std::vector<int>>(size, std::vector<int>())), reversedG(std::vector<std::vector<int>>(size, std::vector<int>())) {}
+
+void ListGraph::AddEdge(int from, int to)
+{
+    g[from].push_back(to);
+    reversedG[to].push_back(from);
 }
 
-void DFS2(int v, std::vector<bool>& used, std::vector<int>& component, std::vector<std::vector<int>>& g2)
+int ListGraph::VerticesCount() const
 {
-    used[v] = true;
-    component.push_back(v);
-    for (int i = 0; i < g2[v].size(); i++)
+    return size;
+}
+
+int ListGraph::NeighboursCount(int vertex) const
+{
+    return g[vertex].size();
+}
+
+void ListGraph::FindAllAdjacentIn(int vertex, std::vector<int>& vertices) const
+{
+    for (int x : reversedG[vertex])
     {
-        if (!used[g2[v][i]])
+        vertices.push_back(x);
+    }
+}
+
+void ListGraph::FindAllAdjacentOut(int vertex, std::vector<int>& vertices) const
+{
+    for (int i : g[vertex])
+    {
+        vertices.push_back(i);
+    }
+}
+
+void DFS1(int v, std::vector<bool>& used, std::vector<int>& order, ListGraph& g1)
+{
+    std::stack<int> stk;
+    stk.push(v);
+    int top;
+    while (!stk.empty())
+    {
+        top = stk.top();
+        stk.pop();
+        used[top] = true;
+        std::vector<int> vertices;
+        g1.FindAllAdjacentIn(top, vertices);
+        for (auto& x : vertices)
         {
-            DFS2(g2[v][i], used, component, g2);
+            if (!used[x])
+            {
+                stk.push(x);
+            }
+        }
+        order.push_back(top);
+    }
+}
+
+void DFS2(int v, std::vector<bool>& used, std::vector<int>& component, ListGraph& g2)
+{
+    std::stack<int> stk;
+    stk.push(v);
+    int top;
+    while (!stk.empty())
+    {
+        top = stk.top();
+        stk.pop();
+        used[top] = true;
+        component.push_back(top);
+        std::vector<int> vertices;
+        g2.FindAllAdjacentIn(top, vertices);
+        for (auto& x : vertices)
+        {
+            if (!used[x])
+            {
+                stk.push(x);
+            }
         }
     }
 }
 
-int main()
+int GetMinAdd(ListGraph& g1, ListGraph& g2, int n)
 {
     std::vector<int> order;
     std::vector<int> component;
     std::vector<std::pair<int, int>> isST;
-    int n, m;
-    std::cin >> n >> m;
-    if (n == 1)
-    {
-        std::cout << 0;
-        return 0;
-    }
-    std::vector<std::vector<int>> g1(n), g2(n);
-    for (int i = 0; i < m; i++)
-    {
-        int a, b;
-        std::cin >> a >> b;
-        if (a == b)
-        {
-            continue;
-        }
-        a--;
-        b--;
-        bool isSecondRoad = false;
-        for (int j = 0; j < g1[a].size(); j++)
-        {
-            if (g1[a][j] == b)
-            {
-                isSecondRoad = true;
-                break;
-            }
-        }
-        if (!isSecondRoad)
-        {
-            g1[a].push_back(b);
-            g2[b].push_back(a);
-        }
-    }
     std::vector<bool> used(n, false);
     for (int i = 0; i < n; i++)
     {
@@ -108,12 +145,14 @@ int main()
     used.assign(n, false);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < g1[i].size(); j++)
+        std::vector<int> vertices;
+        g1.FindAllAdjacentIn(i, vertices);
+        for (auto& x : vertices)
         {
-            if (dsu[i] != dsu[g1[i][j]])
+            if (dsu[i] != dsu[x])
             {
                 isST[dsu[i]].first++;
-                isST[dsu[g1[i][j]]].second++;
+                isST[dsu[x]].second++;
             }
         }
     }
@@ -136,6 +175,47 @@ int main()
             }
         }
     }
-    std::cout << std::max(s + q, t + q) << "\n";
+    return std::max(s + q, t + q);
+}
+
+int main()
+{
+
+    int n, m;
+    std::cin >> n >> m;
+    if (n == 1)
+    {
+        std::cout << 0;
+        return 0;
+    }
+    ListGraph g1(n), g2(n);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        if (a == b)
+        {
+            continue;
+        }
+        a--;
+        b--;
+        bool isSecondRoad = false;
+        std::vector<int> vertices;
+        g1.FindAllAdjacentIn(a, vertices);
+        for (auto& x : vertices)
+        {
+            if (x == b)
+            {
+                isSecondRoad = true;
+                break;
+            }
+        }
+        if (!isSecondRoad)
+        {
+            g1.AddEdge(a, b);
+            g2.AddEdge(b, a);
+        }
+    }
+    std::cout << GetMinAdd(g1, g2, n) << "\n";
     return 0;
 }
